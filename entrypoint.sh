6eb7622b3a590539d26c5480841f1b85289b996a
Ubuntu <ubuntu@ip-172-31-16-147.eu-north-1.compute.internal>
2026-03-31 09:18:37 +0000

    fix 502 gateaway

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -e
 
+echo "⏳ Waiting for PostgreSQL..."
+
+# wait until Postgres is ready
+while ! nc -z db 5432; do
+  sleep 1
+done
+
+echo "✅ PostgreSQL is up, running migrations..."
+
+# run migrations
 python manage.py migrate --noinput
 
+# start Gunicorn
 exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
